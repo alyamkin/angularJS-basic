078862beb46d70723adebc3b86025a1b22781afe
Andrey Lyamkin <andreylyamkin@Andreys-Air.hitronhub.home>
2022-05-31 21:57:19 -0400

    Add ng-switch

--- js/MoviesController.js
+++ js/MoviesController.js
@@ -4,17 +4,21 @@ function MoviesController() {
       title: "The Shawshank Redemption",
       year: "1994",
       popular: true,
+      category: "thriller",
     },
     {
       title: "Inception",
       year: "2010",
       popular: false,
+      category: "fantasy",
     },
     {
       title: "The Matrix",
       year: "1999",
       popular: true,
+      category: "fantasy",
     },
+
     {
       title: "Saving Private Ryan",
       year: "1998",
